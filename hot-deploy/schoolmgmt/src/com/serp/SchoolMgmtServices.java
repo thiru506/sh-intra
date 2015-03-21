03bbd4f50898c851fbf4f9b2d1f279d1e14ec141
nagababut <nagababut@vasista.in>
2015-03-21 18:44:20 +0530

    lib and src added

---
+++ hot-deploy/schoolmgmt/src/com/serp/SchoolMgmtServices.java
@@ -0,0 +1,20 @@
+package com.serp;
+
+import java.math.BigDecimal;
+import java.util.Map;
+
+import org.ofbiz.base.util.Debug;
+import org.ofbiz.base.util.UtilGenerics;
+import org.ofbiz.base.util.UtilMisc;
+import org.ofbiz.base.util.UtilValidate;
+import org.ofbiz.entity.GenericValue;
+import org.ofbiz.service.DispatchContext;
+import org.ofbiz.service.GenericServiceException;
+import org.ofbiz.service.LocalDispatcher;
+import org.ofbiz.service.ServiceUtil;
+
+public class SchoolMgmtServices {
+	public static final String module = SchoolMgmtServices.class.getName();
+    
+
+}
